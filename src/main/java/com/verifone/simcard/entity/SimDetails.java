63f2ec96eccd48132ec7ac478e89738535347cd2
mohit06 <sehgal.mohit06@gmail.com>
2023-11-17 18:40:42 +0530

    Update SimDetails.java

--- src/main/java/com/verifone/simcard/entity/SimDetails.java
+++ src/main/java/com/verifone/simcard/entity/SimDetails.java
@@ -3,12 +3,12 @@ package com.verifone.simcard.entity;
 import java.text.ParseException;
 import java.util.Date;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
 import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class SimDetails {
